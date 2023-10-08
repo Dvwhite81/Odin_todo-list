c82d617486288a0476c2d3df3269245454d317e0
Dvwhite81 <Dvwhite1981@gmail.com>
2023-10-08 17:58:40 -0400

    testing images

--- src/html-elements/sidebar-section.js
+++ src/html-elements/sidebar-section.js
@@ -12,15 +12,15 @@ const buildSideLogo = () => {
   const a = buildElement("a", { id: "side-logo-link" });
   const logo = buildElement("img", {
     id: "side-logo",
-    src: "/dist/images/todo-logo.png",
+    src: "./dist/images/todo-logo.png",
   });
   logo.addEventListener(
     "mouseover",
-    (event) => (event.target.src = "/dist/images/todo-logo-hover.png")
+    (event) => (event.target.src = "./dist/images/todo-logo-hover.png")
   );
   logo.addEventListener(
     "mouseout",
-    (event) => (event.target.src = "/dist/images/todo-logo.png")
+    (event) => (event.target.src = "./dist/images/todo-logo.png")
   );
 
   a.append(logo);
