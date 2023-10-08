ccce5d7b945994d5eb9fc647ef131e68b5c0fa9b
Dvwhite81 <Dvwhite1981@gmail.com>
2023-10-08 15:10:27 -0400

    changed some settings in package and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 };
+
+module: {
+  rules: [
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
+      }
+    }
+  ]
+}
